af24bab2c47d1517aeac5676b69dede3b95b6d88
Reming <927627286@qq.com>
2019-09-27 17:19:34 +0800

    feat: add header

---
+++ src/home/view/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import AppHeader from './components/app-header/index';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <AppHeader></AppHeader>
+    </div>
+  );
+}
+
+export default App;
