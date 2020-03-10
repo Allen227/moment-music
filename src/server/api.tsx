580f1abcdd7e3778b033a6962ec4dc2738819038
Reming <927627286@qq.com>
2020-03-10 23:46:12 +0800

    feat: add fetch song word

--- src/server/api.tsx
+++ src/server/api.tsx
@@ -7,7 +7,8 @@ const Api = {
   fetch_recommend_tags_detail: (cat: string) => req(`${server_url}/top/playlist?limit=30&cat=${cat}`, 'GET'),
   fetch_playlist_detail: (playlistId: number) => req(`${server_url}/playlist/detail?id=${playlistId}`, 'GET'),
   fetch_song_url: (songId: number) => req(`${server_url}/song/url?id=${songId}`, 'GET'),
-  fetch_rank_list: () => req(`${server_url}/toplist`, 'GET')
+  fetch_rank_list: () => req(`${server_url}/toplist`, 'GET'),
+  fetch_song_word: (songId: number) => req(`${server_url}/lyric?id=${songId}`, 'GET')
 }
 
 export default Api;
